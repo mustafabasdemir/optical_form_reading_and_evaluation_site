e7584a5d9cef0a9ee2ebac35adc839986945016d
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2022-11-16 21:22:27 +0300

    Delete proje/application directory

--- proje/application/views/ortksyf/altbilgi.php
+++
@@ -1,5 +0,0 @@
-
-<div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.3
-</div>
-<strong><center>Bilecik Şeyh Edebali Üniversitesi - Uzem </center></strong>
--- proje/application/views/ortksyf/logoisim.php
+++
@@ -1,6 +0,0 @@
-
-
-     <a href="#" class="logo" style="background-color: #222d32">
-	<span class="logo-mini"><img src="/proje/img/bseu.png"></span>
-      <span class="logo-lg">BŞEÜ UZEM</span>
-    </a>
